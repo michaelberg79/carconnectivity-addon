9da22c1e59d8a70db4ebaa1727ce3ba606729f33
Pulpyyyy <naboleo@gmail.com>
2025-03-14 20:35:56 +0100

    synch from beta

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 set -e
+
+# Function to handle signals
+term_handler() {
+    echo "SIGTERM signal received, shutting down..."
+    if [ -n "$child_pid" ] && kill -0 "$child_pid" 2>/dev/null; then
+        kill -TERM "$child_pid"
+        wait "$child_pid"
+    fi
+    exit 143 # 128 + 15 -- SIGTERM
+}
+
+# trap SIGTERM - sent when 'docker stop'
+trap 'term_handler' TERM
+
 echo ""
 echo ""
 echo ""
 echo ">>>>>>>>> STARTING"
 cd /tmp
 tempio -conf /data/options.json -template carconnectivity.json.gtpl -out carconnectivity.json
+
 if grep -q "debug" /data/options.json; then
     for file in versions.txt carconnectivity.json; do
         echo ">>>>>>>>> $(basename "$file")"
@@ -13,5 +28,8 @@ if grep -q "debug" /data/options.json; then
         echo "<<<<<<<<<<" 
     done
 fi
-/opt/venv/bin/carconnectivity carconnectivity.json
+
+/opt/venv/bin/carconnectivity carconnectivity.json &
+child_pid=$!
 echo ">>>>>>>>> STARTED"
+wait "$child_pid"
